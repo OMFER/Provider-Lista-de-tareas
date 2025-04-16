09dd6752f2989209d53b6086cce774288dec1e4a
Fernando Otáñez <omfer3.0@gmail.com>
2025-04-16 16:43:55 -0600

    <feature>(prov) Agregando provider

--- lib/providers/tarea_prov.dart
+++ lib/providers/tarea_prov.dart
@@ -2,3 +2,39 @@ import 'package:flutter/material.dart';
 import '../models/model_tarea.dart';
 import '../services/api_service.dart';
 
+class TareaProvider with ChangeNotifier {
+    final ApiService apiService;
+    List<Tarea> _tareas = [];
+
+    TareaProvider({required this.apiService});
+
+    List<Tarea> get tareas => _tareas;
+
+    Future<void> cargarTareas() async {
+      _tareas = await apiService.getTareas();
+      notifyListeners();
+    }
+
+    Future<Tarea> crearTarea(Tarea tarea) async {
+      final nuevaTarea = await apiService.createTarea(tarea);
+      _tareas.add(nuevaTarea);
+      notifyListeners();
+      return nuevaTarea;
+    }
+
+    Future<Tarea> actualizarTarea(Tarea tarea) async {
+      final tareaActualizada = await apiService.updateTarea(tarea);
+      final index = _tareas.indexWhere((t) => t.id == tarea.id);
+      if (index != -1) {
+        _tareas[index] = tareaActualizada;
+        notifyListeners();
+      }
+      return tareaActualizada;
+    }
+
+    Future<void> eliminarTarea(int id) async {
+      await apiService.deleteTarea(id);
+      _tareas.removeWhere((t) => t.id == id);
+      notifyListeners();
+    }
+}
